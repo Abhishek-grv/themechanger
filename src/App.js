91c5afad623c592e70c266e1ee4c5e30ffa3fdea
Abhishek Gaurav <gauravabhishek1311@gmail.com>
2024-07-28 09:43:18 +0530

    first comment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Posts from "./components/Posts";
+import { ThemeContext, themes } from "./context/themeContext";
 
 function App() {
+  const [theme, setTheme] = useState(themes.light);
+
+  // btn
+  function handleOnClick() {
+    theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light);
+  }
+
+  useEffect(() => {
+    const body = document.body;
+    switch (theme) {
+      case themes.light:
+        body.classList.remove("bg-dark", "text-light");
+        body.classList.add("bg-light", "text-dark");
+        break;
+      case themes.dark:
+        body.classList.remove("bg-light", "text-dark");
+        body.classList.add("bg-dark", "text-light");
+        break;
+      default:
+        body.classList.remove("bg-dark", "text-light");
+        body.classList.add("bg-light", "text-dark");
+    }
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, handleOnClick }}>
+      <div className="main-container">
+        <h1 className="text-center">Light Dark Theme App</h1>
+        <Posts theme={theme} />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
